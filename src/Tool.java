a68fc1311010ea1cd9be17963d6efa5ebaa7eb98
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-04-24 10:05:34 -0400

    added in method names for the Tool class

--- src/Tool.java
+++ src/Tool.java
@@ -1,9 +1,53 @@
 public abstract class Tool {
     
-    public Tool() {
+    private Condition condition;
+    private int age;
+    private boolean checkedOut;
 
-        
+    public Tool(Condition condition, int age, boolean checkedOut) {
+
+        this.condition = condition;
+        this.age = age;
+        this.checkedOut = checkedOut;
 
     }
+
+    // enum for conditions 
+    public enum Condition {
+
+        Perfect,
+        Good,
+        Average,
+        Bad,
+        Awful
+
+    }
+
+    /**
+     * calculates how urgent a tool needs to be replaced 
+     * 
+     * @return how urgent the tool needs to be replaced 
+     */
+    public String getReplacementUrgency() {
+
+        return null;
+
+    }
+
+    /**
+     * checks if the tool is checked out 
+     * 
+     * @return true if it is checked out 
+     */
+    public boolean isCheckedOut() {
+
+        return checkedOut;
+
+    }
+
+    // abstract classes
+    public abstract String getMaintenance();
+    public abstract String getFunction();
+    public abstract String toString();
 
 }
